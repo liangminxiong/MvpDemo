e161bdd79c9608b8b99448ca3134fa402ad4a5d1
liangminxiong <1409318524@qq.com>
2018-11-20 17:28:20 +0800

    首页 使用Viewpager fragment

---
+++ app/src/main/java/com/project/lmx/mvpmedo/ui/activity/ViewPagerMainActivity.java
@@ -0,0 +1,22 @@
+package com.project.lmx.mvpmedo.ui.activity;
+
+
+import android.os.Bundle;
+
+import com.project.lmx.sdk.base.activity.BaseCompatActivity;
+
+/*viewpager fragment tablayout */
+public class ViewPagerMainActivity extends BaseCompatActivity {
+
+
+
+    @Override
+    protected void initView(Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return 0;
+    }
+}
